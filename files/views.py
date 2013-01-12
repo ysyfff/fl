98763dd4ccd88624e22e7eacc2b31fe61e9807f9
ysy <ysy@ysy.(none)>
2013-01-12 20:13:18 +0800

    Sample

---
+++ files/views.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+from django.template import Context
+from django.template import loader
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.contrib.auth.models import User
+import datetime
+import time
+from django.utils import timezone
+
+def home(request):
+    return render_to_response('files/home.html',
+        locals(),
+        context_instance=RequestContext(request)
+    )
