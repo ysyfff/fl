72faeb31116faf2dd96013bc2208b0d25c8a5793
ysyfff <1156546473@qq.com>
2013-04-26 23:47:54 +0800

    从前台实现了对上传文件名作更改

--- files/models.py
+++ files/models.py
@@ -2,5 +2,4 @@ from django.db import models
 
 class Car(models.Model):
     name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
     photo = models.ImageField(upload_to='headimg')
--- files/views.py
+++ files/views.py
@@ -12,17 +12,33 @@ from django.contrib.auth.models import User
 import datetime
 import time
 from django.utils import timezone
+from datetime import datetime
 from files.forms import UploadFileForm
 from files.models import Car
 
+ROOT_PATH = '/static/img/headimg/'
+
 def home(request):
+    file_name = datetime.now().strftime('%Y%m%d%H%M%S')
     if request.method=="POST":
         photo = request.FILES['file']
-        car = Car(name="aaa", price=12, photo=photo)
+        p_name, format = photo.name.split('.')
+        photo.name=file_name+'.'+format
+        car = Car(name=photo.name, photo=photo)
         car.save()
     else:
         form = UploadFileForm()
+
+    car = Car.objects.all()
+    car_content = []
+    for c in car:
+        car_content.append({
+            'c_name': c.name,
+            'c_url': ROOT_PATH+c.name,
+        })
     return render_to_response('files/home.html',
         locals(),
         context_instance=RequestContext(request)
-    )
+        )
+
+    
