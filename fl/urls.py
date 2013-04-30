20c5df3db6c62afa86aabff7d64f1ad735f2f587
ysyfff <1156546473@qq.com>
2013-04-30 22:47:14 +0800

    解决了不能访问media路径下的文件的问题

--- fl/urls.py
+++ fl/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf import settings
+from django.conf.urls.static import static
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -16,4 +17,4 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.home),
     url(r'^files/', include('files.urls')),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
